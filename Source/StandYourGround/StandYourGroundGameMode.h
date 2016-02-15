f1fb6228ec6ff78cfef37b6ec30544f3beac3868
lujeff <jefflu@live.ca>
2016-02-14 23:52:16 -0500

    Implemented C++ project files

---
+++ Source/StandYourGround/StandYourGroundGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "StandYourGroundGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STANDYOURGROUND_API AStandYourGroundGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
